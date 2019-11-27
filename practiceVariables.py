8bd52dcad92fe494b1fd829fb0b9e4bcd4411cab
NathanBoedecker <57597199+NathanBoedecker@users.noreply.github.com>
2019-11-26 19:57:13 -0600

    Initial commit

---
+++ practiceVariables.py
@@ -0,0 +1,37 @@
+'''
+Created on Nov 10, 2019
+
+@author: ITAUser
+'''
+
+a = 4
+
+b = 3.45
+
+c = "string"
+
+a = True
+
+listInt = [1,2,3,4,5]
+
+print(listInt[3])
+
+listTwo = ["3","4","5"]
+
+listThree = [True, True, False]
+
+'''This is a boolean'''
+var = listThree[2]
+
+'''this is a list'''
+
+var = listThree
+
+string = "michael"
+
+name = string[0:3]
+print(name)
+name = string[:5]
+print(name)
+name = string[5:]
+print(name)
